47a747a2e29f966f7805535dd768fd2c0103b771
markup54 <marzenakupka@tlen.pl>
2021-10-04 10:23:18 +0200

    listener

--- app/src/main/java/com/example/przepisy/MainActivity.java
+++ app/src/main/java/com/example/przepisy/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.przepisy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdapterView.OnItemClickListener wybrany =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(
+                            AdapterView<?> adapterView,
+                            View view,
+                            int i,
+                            long l) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "klinieto "+Integer.toString(i),
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                };
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(wybrany);
     }
 }

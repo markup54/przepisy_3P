ee0d52cb7b411c02a8ee1873181f6dbf26f2a221
markup54 <marzenakupka@tlen.pl>
2021-10-11 09:17:17 +0200

    przepisActiwvity

---
+++ app/src/main/java/com/example/przepisy/PrzepisActivity.java
@@ -0,0 +1,14 @@
+package com.example.przepisy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrzepisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_przepis);
+    }
+}

54711a6b9455088722dd945e59954656a9fab7c0
markup54 <marzenakupka@tlen.pl>
2021-10-11 10:36:38 +0200

    dodanie dynamicznego wyswietlania widoku

--- app/src/main/java/com/example/przepisy/PrzepisActivity.java
+++ app/src/main/java/com/example/przepisy/PrzepisActivity.java
@@ -3,6 +3,10 @@ package com.example.przepisy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PrzepisActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class PrzepisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_przepis);
+        int id_kat=(int)getIntent()
+                .getExtras()
+                .get(ListaPrzepisowActivity.EXTRA_KAT);
+        int id_przepisu = (int)getIntent()
+                .getExtras()
+                .get(ListaPrzepisowActivity.EXTRA_ID);
+        ArrayList<Przepis> wybrane=
+                RepozytoriumPrzepisow.getPrzepisy(id_kat);
+        Przepis wyswietlanyPrzepis =
+                wybrane.get(id_przepisu);
+        TextView nazwaTextView =
+                (TextView) findViewById(R.id.textView3);
+        nazwaTextView.setText(wyswietlanyPrzepis.getNazwa());
+        ImageView imageView=
+                (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(wyswietlanyPrzepis.getIdZdjecia());
+        imageView.setContentDescription(wyswietlanyPrzepis.getNazwa());
+        TextView skladnikiTextView=
+                (TextView) findViewById(R.id.textView4);
+        skladnikiTextView.setText(wyswietlanyPrzepis.getSkladniki());
+
     }
 }

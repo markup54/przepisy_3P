2e55470118f1019a42c8b0d0db66aad372778001
markup54 <marzenakupka@tlen.pl>
2021-10-11 08:21:32 +0200

    repozytorium przepisów

--- app/src/main/java/com/example/przepisy/RepozytoriumPrzepisow.java
+++ app/src/main/java/com/example/przepisy/RepozytoriumPrzepisow.java
@@ -4,6 +4,20 @@ public class RepozytoriumPrzepisow {
     private static final Przepis[] przepisy={
             new Przepis("Muffinki",
                     1,
+                    R.drawable.muf,
+                    "jajka, mąka, cukier, mleko, olej, kakao"),
+            new Przepis("Gofry",
+                    1,
+                    R.drawable.gofr,
+                    "jajka, mleko, mąka, cukier"),
+            new Przepis("Pączki",
+                    1,
+                    R.drawable.pocz,
+                    "drożdże, masło, mleko, cukier, mąka"),
+            new Przepis("Kawa",
+                    0,
+                    R.drawable.kawa,
+                    "kawa, woda"
                     )
-    }
+    };
 }

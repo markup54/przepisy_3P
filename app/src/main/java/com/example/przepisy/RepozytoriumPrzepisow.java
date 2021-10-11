b6efcd3d920ef136f5f7c0bf7faff823c7e8c689
markup54 <marzenakupka@tlen.pl>
2021-10-11 08:12:28 +0200

    zdj

---
+++ app/src/main/java/com/example/przepisy/RepozytoriumPrzepisow.java
@@ -0,0 +1,9 @@
+package com.example.przepisy;
+
+public class RepozytoriumPrzepisow {
+    private static final Przepis[] przepisy={
+            new Przepis("Muffinki",
+                    1,
+                    )
+    }
+}
